8dadd46856db4390d52dc498acc651ccc843ca13
Kevin Suen <kevinsuen11@gmail.com>
2019-09-19 10:39:24 -0400

    Implement category pages for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  get '/about', to: 'about#show'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -12,11 +13,11 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
   
-  get '/about', to: 'about#show'
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
 
 
   end
