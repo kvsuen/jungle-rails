35f14d42804743a6513c7992a8563a0a3c7cc832
Kevin Suen <kevinsuen11@gmail.com>
2019-09-19 14:26:33 -0400

    Implement user authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   root to: 'products#index'
   get '/about', to: 'about#show'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
